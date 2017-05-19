79e736a6389ea5b29ae14ca28cf64a7cae6e5a44
wangxiao <wangxiao@chiway.com.cn>
2017-05-19 16:49:24 +0800

    Initial commit

---
+++ basic/src/main/java/com/springapp/mvc/HelloController.java
@@ -0,0 +1,18 @@
+package com.springapp.mvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class HelloController {
+	//@RequestMapping(method = RequestMethod.GET)
+    //@RequestMapping("hello")
+    @RequestMapping("/")
+	public String printWelcome(ModelMap model) {
+		model.addAttribute("message", "Hello world!");
+        System.out.println("hello");
+        return "hello";
+	}
+}
