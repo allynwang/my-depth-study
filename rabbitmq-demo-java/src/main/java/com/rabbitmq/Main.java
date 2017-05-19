1bc45b0059c342bd512c50f591eef1f558eacf63
wangxiao <wangxiao@chiway.com.cn>
2017-05-19 17:02:32 +0800

    rabbit

---
+++ rabbitmq-demo-java/src/main/java/com/rabbitmq/Main.java
@@ -0,0 +1,36 @@
+package com.rabbitmq;
+
+import java.util.HashMap;
+
+/**
+ * 测试主类
+ * Created by honestAnt on 2017/5/17.
+ */
+public class Main {
+    public Main() throws Exception{
+
+        QueueConsumer consumer = new QueueConsumer("queue");
+        Thread consumerThread = new Thread(consumer);
+        consumerThread.start();
+
+        Producer producer = new Producer("queue");
+
+        for (int i = 0; i < 100000; i++) {
+            HashMap message = new HashMap();
+            message.put("message number", i);
+            producer.sendMessage(message);
+            System.out.println("Message Number "+ i +" sent.");
+        }
+        System.out.println("test");
+        System.out.println("sss");
+    }
+
+    /**
+     * @param args
+     * @throws SQLException
+     * @throws IOException
+     */
+    public static void main(String[] args) throws Exception{
+        new Main();
+    }
+}
